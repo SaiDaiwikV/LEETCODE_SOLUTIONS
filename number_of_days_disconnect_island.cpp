033e70e1e01a08220bb9d0ba4bf674c75e19a9fc
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2024-08-11 09:39:34 +0530

    Add files via upload

---
+++ number_of_days_disconnect_island.cpp
@@ -0,0 +1,89 @@
+/*You are given an m x n binary grid grid where 1 represents land and 0 represents water. An island is a maximal 4-directionally (horizontal or vertical) connected group of 1's.
+
+The grid is said to be connected if we have exactly one island, otherwise is said disconnected.
+
+In one day, we are allowed to change any single land cell (1) into a water cell (0).
+
+Return the minimum number of days to disconnect the grid.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1,1,0],[0,1,1,0],[0,0,0,0]]
+
+Output: 2
+Explanation: We need at least 2 days to get a disconnected grid.
+Change land grid[1][1] and grid[0][2] to water and get 2 disconnected island.
+Example 2:
+
+
+Input: grid = [[1,1]]
+Output: 2
+Explanation: Grid of full water is also disconnected ([[1,1]] -> [[0,0]]), 0 islands.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 30
+grid[i][j] is either 0 or 1.*/
+
+// sloution : 
+
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDays(vector<vector<int>>& grid) {
+        // Lambda function to count the number of islands in the grid
+        auto count_islands = [&]() -> int {
+            vector<vector<int>> seen(grid.size(), vector<int>(grid[0].size(), 0));
+            int islands = 0;
+            
+            // Depth-First Search (DFS) to explore and mark all cells of an island
+            function<void(int, int)> dfs = [&](int r, int c) {
+                seen[r][c] = 1;
+                for (auto [dr, dc] : {pair{-1, 0}, {1, 0}, {0, -1}, {0, 1}}) {
+                    int nr = r + dr, nc = c + dc;
+                    if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && grid[nr][nc] == 1 && !seen[nr][nc]) {
+                        dfs(nr, nc);
+                    }
+                }
+            };
+            
+            // Iterate through the grid to find islands
+            for (int i = 0; i < grid.size(); i++) {
+                for (int j = 0; j < grid[0].size(); j++) {
+                    if (grid[i][j] == 1 && !seen[i][j]) {
+                        islands++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            return islands;
+        };
+        
+        // If the initial number of islands is not 1, return 0 (already disconnected)
+        if (count_islands() != 1) return 0;
+        
+        // Try removing each land cell to see if it disconnects the island
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 0;  // Temporarily remove the land cell
+                    if (count_islands() != 1) return 1;  // If not 1 island, return 1
+                    grid[i][j] = 1;  // Restore the land cell
+                }
+            }
+        }
+        
+        // If no single removal disconnects the island, return 2
+        return 2;
+    }
+};
