c6b716916b2d2a7f7049d1aa03c20f3c1bf3aa4f
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2025-02-10 12:05:14 +0530

    Time: 1 ms (25.26%), Space: 9.2 MB (14.68%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int n = s.length();
+        stack<char> st;
+
+        for(char c : s){
+            if(isdigit(c)){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }else{
+                st.push(c);
+            }
+        }
+        string res;
+        while(!st.empty()){
+            res += st.top();
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
