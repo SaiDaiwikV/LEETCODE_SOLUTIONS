62714721d95170b1ae46e563c6dad7f001597fcf
V. SAI DAIWIK <113048796+SaiDaiwikV@users.noreply.github.com>
2025-02-10 12:12:06 +0530

    Time: 3 ms (15.28%), Space: 8.6 MB (56.73%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,22 +1,39 @@
+// class Solution {
+// public:
+//     string clearDigits(string s) {
+//         stack<char> st;
+//         for(int i=0;i<s.length();i++){
+//             if(!isdigit(s[i])){
+//                 st.push(s[i]);
+//             }
+//             else{
+//                 st.pop();
+//             }
+//         }
+//         string ans="";
+//         while(!st.empty()){
+//             ans+=st.top();
+//             st.pop();
+//         }
+//         reverse(ans.begin(),ans.end());
+//         return ans;
+        
+//     }
+// };
+
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char> st;
-        for(int i=0;i<s.length();i++){
-            if(!isdigit(s[i])){
-                st.push(s[i]);
-            }
-            else{
-                st.pop();
+        string updatestr = "";
+        for(int i = 0;i<s.length();i++){
+            if(isdigit(s[i])){
+                if(!updatestr.empty()){
+                    updatestr.pop_back();
+                }
+            }else{
+                updatestr.push_back(s[i]);
             }
         }
-        string ans="";
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
-        
+        return updatestr;
     }
 };
